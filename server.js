8939c6cbcbb8d8987e93ac1a5974d022ea864954
MSNimo <mikebenim@gmail.com>
2019-01-29 15:40:11 -0600

    Initial Push

---
+++ server.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var path = require('path');
+var bodyParser = require('body-parser');
+
+server.listen(process.env.PORT || 397, function(){
+    console.log('listening on port 397');
+});
+
+app.set('view engine');
+app.set('views', __dirname + '');
+app.use(express.static(path.join(__dirname, '')));
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', function(req, res){
+  res.render('index'); 
+});
+
+
+
